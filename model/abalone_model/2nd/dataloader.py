faf2bc08cabb6800a68c8fa00b94361f0400e46d
KimBS <born9608@gmail.com>
2023-07-24 10:39:15 +0900

    split_data_load

--- model/abalone_model/2nd/dataloader.py
+++ model/abalone_model/2nd/dataloader.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 # 데이터셋을 불러온 뒤 이상치를 제거하는 함수
 # 원데이터의 경로를 csv_path에 입력하면 전처리된 데이터프레임을 반환
-def load_prepro_data(csv_path='Regression_data.csv'):
+# default Train:Valid:Test = 6:2:2
+# split random_state 필요하면 설정하기
+
+def split_dataloader(csv_path='Regression_data.csv', Train_Ratio = 0.6):
     # original 데이터셋 불러오기
     df = pd.read_csv(csv_path)
 
@@ -34,4 +39,10 @@ def load_prepro_data(csv_path='Regression_data.csv'):
     df_clean = df_origin.copy()
     df_clean.iloc[:, 1:8] /= 200
     
-    return df_clean
+    # 학습 데이터셋과 나머지를 나누기
+    train_data, temp_data = train_test_split(df_clean, train_size=Train_Ratio)
+
+    # 나머지 데이터를 검증 데이터셋과 테스트 데이터셋으로 나누기
+    valid_data, test_data = train_test_split(temp_data, train_size=0.5)
+
+    return train_data, valid_data, test_data
